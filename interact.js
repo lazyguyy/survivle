e8d4dab8f735353431859a2f9f3fa096f8f46429
lazyguyy <17795787+lazyguyy@users.noreply.github.com>
2022-02-23 15:25:49 +0100

    Rebrand to Survivle

--- interact.js
+++ interact.js
@@ -24,15 +24,13 @@ function computeScore(target, entered) {
 }
 
 window.onload = function() {
-    var boards = [document.getElementById("first-board"), document.getElementById("second-board")]
+    var boards = document.getElementById("board")
     var word = ""
     var notifications = document.getElementById("notifications")
     var valid_words = new Set(["hallo", "crazy", "upset"])
-    var target_words = ["wuble", "allas"]
-    var target_length = target_words[0].length
-    for (const board of boards) {
-        createNewRow(board)
-    }
+    var target_word = "wuble"
+    var target_length = target_word.length
+    createNewRow()
     window.addEventListener("keydown", event => react(event.key))
 
 
@@ -79,7 +77,7 @@ window.onload = function() {
     }
 
 
-    function updateBoard(board) {
+    function updateBoard() {
         notifications.textContent = ""
         let row = board.rows.item(board.rows.length - 1)
         for (var i = 0; i < target_length; ++i) {
@@ -98,20 +96,16 @@ window.onload = function() {
             return
         }
 
-        for (let b = 0; b < 2; ++b) {
-            target_word = target_words[b]
-            board = boards[b]
-            score = computeScore(target_word, word.toLowerCase())
-            let row = board.rows.item(board.rows.length - 1)
-            for (var i = 0; i < target_word.length; ++i) {
-                row.children[i].className = ["gray", "yellow", "green"][score[i]]
-            }
-            createNewRow(board)
+        score = computeScore(target_word, word.toLowerCase())
+        let row = board.rows.item(board.rows.length - 1)
+        for (var i = 0; i < target_word.length; ++i) {
+            row.children[i].className = ["gray", "yellow", "green"][score[i]]
         }
+        createNewRow()
         word = ""
     }
 
-    function createNewRow(board) {
+    function createNewRow() {
         row = board.insertRow(board.rows.length)
         row.className = "board-row"
         row.innerHTML = "<td></td>".repeat(target_length)
@@ -121,9 +115,7 @@ window.onload = function() {
     function react(key) {
         if (key == "Backspace"){
             word = word.slice(0, -1)
-            for (const board of boards) {
-                updateBoard(board)
-            }
+            updateBoard()
             return
         }
 
@@ -134,9 +126,7 @@ window.onload = function() {
         if (word.length < 5 && key.length == 1 && (/[a-zA-Z]/).test(key)) {
             key = key.toUpperCase()
             word += key
-            for (const board of boards) {
-                updateBoard(board)
-            }
+            updateBoard()
         }
     }
 }
