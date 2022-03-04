d3c32952398bb8a62dee66c97126f630c01da141
lazyguyy <17795787+lazyguyy@users.noreply.github.com>
2022-03-04 12:11:16 +0100

    looks better now I think

--- interact.js
+++ interact.js
@@ -52,21 +52,21 @@ function uses_info(word, hints){
             return false
         }
         if (hints.blocked_positions[i].has(word[i])) {
-            notifications.textContent = `The ${ordinals[i + 1]} letter is not ${word[i].toUpperCase()}.`
+            notifications.textContent = `The ${ordinals[i + 1]} letter cannot be ${word[i].toUpperCase()}.`
             return false
         }
     }
     for (const letter of Object.keys(hints.letter_counts)) {
         if (hints.letter_counts[letter] > letter_counts[letter]) {
-            notifications.textContent = `${letter.toUpperCase()} is contained ${hints.gray_letters.has(letter) ? "exactly" : "at least"} ${numerus(hints.letter_counts[letter], "time", "times")}.`
+            notifications.textContent = `The word contains the letter ${letter.toUpperCase()} ${hints.gray_letters.has(letter) ? "exactly" : "at least"} ${numerus(hints.letter_counts[letter], "time", "times")}.`
             return false
         }
         if (hints.gray_letters.has(letter) && letter_counts[letter] > hints.letter_counts[letter]) {
             if (hints.letter_counts[letter] == 0) {
-                notifications.textContent = `${letter.toUpperCase()} is not contained.`
+                notifications.textContent = `The word does not contain the letter ${letter.toUpperCase()}.`
 
             } else {
-                notifications.textContent = `${letter.toUpperCase()} is contained only ${numerus(hints.letter_counts[letter], "time", "times")}.`
+                notifications.textContent = `The word contains the letter ${letter.toUpperCase()} only ${numerus(hints.letter_counts[letter], "time", "times")}.`
             }
             return false
         }
@@ -216,7 +216,7 @@ function submitWord(board, word, hints) {
         row.children[i].className = ["gray", "yellow", "green"][score[i]]
     }
     if (score.every(x => x == 2)) {
-        notifications.textContent = `You survived for ${board.rows.length} rounds.`
+        notifications.textContent = `You survived for ${board.rows.length} rounds. Congratulations!`
         solved = true
         return false
     }
